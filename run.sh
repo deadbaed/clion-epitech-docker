8ddd8a666f923fa7c796bed2402123d24d71166b
Philippe Loctaux <p@philippeloctaux.com>
2021-03-09 16:32:50 +0100

    build docker image only if it does not exist already

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@
 # define port to use
 PORT=2222
 
-# build the docker image
-docker build -t clion-epitech-docker -f Dockerfile .
+# image
+IMAGE_NAME=clion-epitech-docker
+
+# build the docker image if it does not exist
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+  docker build -t $IMAGE_NAME -f Dockerfile .
+fi
 
 # run the docker container
-docker run --rm -d --cap-add sys_ptrace -p127.0.0.1:$PORT:22 --name clion_remote_env_epitech clion-epitech-docker
+docker run --rm -d --cap-add sys_ptrace -p127.0.0.1:$PORT:22 --name clion_remote_env_epitech $IMAGE_NAME
 
 # clear ssh keys if there was another ssh server on the same port
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:$PORT"
